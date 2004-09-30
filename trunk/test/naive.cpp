a085209be870db0281a2735bd8f78037b9653474
heisenbug <heisenbug@a318f3ce-71e3-817b-b164-db38ee571ebd>
2004-09-30 16:35:10 +0000

    checkpoint, compiles

--- trunk/test/naive.cpp
+++ trunk/test/naive.cpp
@@ -25,6 +25,25 @@ namespace aL4nin
     }
 }
 
+// metadata entry defines the
+// marking method:
+// a) bitpattern (up to 31 bits representing pointers to follow)
+// b) freestyle procedural marker
+//     - uncommitted arrays must zero out object data before setting marker
+//     - concurrency in data <--> metadata access???
+
+
+struct foo
+{
+    foo& f;
+    foo() : f(*this)
+        {}
+    void bar()
+        {
+///            int i(&foo::f);
+        }
+    
+};
 
 
 
@@ -39,4 +58,5 @@ int main(void)
 {
     vector<int, alloc<int> > v(3);
     map<int, int, less<int>, alloc<int> > m;
+////    m.insert(make_pair(1, 42));
 }
