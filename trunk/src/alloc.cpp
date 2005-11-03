c0537214cef4cf8f9889ab14b6604e15d9966506
heisenbug <heisenbug@a318f3ce-71e3-817b-b164-db38ee571ebd>
2005-11-03 17:46:14 +0000

    update copyright

--- trunk/src/alloc.cpp
+++ trunk/src/alloc.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004 Gabor Greif
+ * Copyright (c) 2004-2005 Gabor Greif
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
