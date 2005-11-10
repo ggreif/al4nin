dbe617fa9456182f18df3b52fb1197c77c815a36
heisenbug <heisenbug@a318f3ce-71e3-817b-b164-db38ee571ebd>
2005-11-10 02:01:09 +0000

    add copyright notice minor cleanup

--- test/naive.cpp
+++ test/naive.cpp
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2004-2005 Gabor Greif
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <vector>
 #include <map>
 #include <cstring>
@@ -254,15 +277,12 @@ inline const void* RawObj2Meta(const void* obj)
     enum 
         {
             pat = (1 << PAGE + CLUSTER) - 1,
-///            irrel = (1 << GRAN+SCALE) - 1
             mask = ~((1 << GRAN) - 1)
         };
     
     register sptr_t b(o & ~static_cast<sptr_t>(pat));
     register sptr_t d(o & static_cast<sptr_t>(pat));
     return reinterpret_cast<const void*>(b + ((d >> SCALE) & mask));
-///    register sptr_t d(o & static_cast<sptr_t>(pat) & ~irrel);
-///    return reinterpret_cast<const void*>(b + (d >> SCALE));
 }
 
 
