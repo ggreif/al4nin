b25fd489462b4252fab6aa14a8f07e851d1adaad
heisenbug <heisenbug@a318f3ce-71e3-817b-b164-db38ee571ebd>
2006-07-26 09:45:28 +0000

    Added tabling, fixed a bug

--- purgatory/terraincognita/um.cpp
+++ purgatory/terraincognita/um.cpp
@@ -39,13 +39,33 @@ void Halt(Fun* array0, unsigned (&regs)[10])
 
 typedef typeof(Halt) *Instruct;
 
+#define genC(NAME, A, B) NAME<A, B, 0>, NAME<A, B, 1>, NAME<A, B, 2>, NAME<A, B, 3>, NAME<A, B, 4>, NAME<A, B, 5>, NAME<A, B, 6>, NAME<A, B, 7>
+#define genB(NAME, A) genC(NAME, A, 0), genC(NAME, A, 1), genC(NAME, A, 2), genC(NAME, A, 3), genC(NAME, A, 4), genC(NAME, A, 5), genC(NAME, A, 6), genC(NAME, A, 7)
+#define genA(NAME) genB(NAME, 0), genB(NAME, 1), genB(NAME, 2), genB(NAME, 3), genB(NAME, 4), genB(NAME, 5), genB(NAME, 6), genB(NAME, 7)
+
 template <int A, int B, int C>
 void Add(Fun* array0, unsigned (&regs)[10])
 {
-    std::cerr << "Adding. [" << A << "] = [" << B << "] + [" << C << "]" << std::endl;
+//    std::cerr << "Adding. [" << A << "] = [" << B << "] + [" << C << "]" << std::endl;
     regs[A] = regs[B] + regs[C];
     ++array0;
-    (*reinterpret_cast<typeof(&Halt)*>(array0))(array0, regs);
+    (*reinterpret_cast<Instruct*>(array0))(array0, regs);
+}
+
+template <int A, int B, int C>
+void Mul(Fun* array0, unsigned (&regs)[10])
+{
+    regs[A] = regs[B] * regs[C];
+    ++array0;
+    (*reinterpret_cast<Instruct*>(array0))(array0, regs);
+}
+
+template <int A, int B, int C>
+void Div(Fun* array0, unsigned (&regs)[10])
+{
+    regs[A] = regs[B] / regs[C];
+    ++array0;
+    (*reinterpret_cast<Instruct*>(array0))(array0, regs);
 }
 
 void Compiler(Fun* array0, unsigned (&regs)[10])
@@ -60,8 +80,20 @@ void Compiler(Fun* array0, unsigned (&regs)[10])
         case 0:;
         case 3:
         {
-            typeof(&Halt) adders[8 * 8 * 8] = { Add<0, 0, 0>, Add<0, 0, 1> };
-            array0[0] = reinterpret_cast<Fun>(adders[platter & 0x1F]);
+            Instruct const adders[8 * 8 * 8] = { genA(Add) };
+            array0[0] = reinterpret_cast<Fun>(adders[platter & 0x1FF]);
+            break;
+        };
+        case 4:
+        {
+            Instruct const multipliers[8 * 8 * 8] = { genA(Mul) };
+            array0[0] = reinterpret_cast<Fun>(multipliers[platter & 0x1FF]);
+            break;
+        };
+        case 5:
+        {
+            Instruct const dividers[8 * 8 * 8] = { genA(Div) };
+            array0[0] = reinterpret_cast<Fun>(dividers[platter & 0x1FF]);
             break;
         };
         case 7:
@@ -69,7 +101,7 @@ void Compiler(Fun* array0, unsigned (&regs)[10])
             break;
     };
     
-    (*reinterpret_cast<typeof(&Halt)*>(array0))(array0, regs);
+    (*reinterpret_cast<Instruct*>(array0))(array0, regs);
 }
 
 
@@ -95,7 +127,7 @@ int main(void)
     std::cerr << "main." << std::endl;
     DylanVector& v(*(DylanVector*)new char[100]);
     v.size = 2;
-    v.arr[0].data = (3 << 28) + 1; // Add: A = 0, B = 0, C = 1
+    v.arr[0].data = (3 << 28) + (5 << 6) + (3 << 3) + 1; // Add: A = 5, B = 3, C = 1
     v.arr[1].data = 7 << 28;       // Halt
     
     enterUM(NULL, v);
