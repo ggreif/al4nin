c563d8e3f56d704d7ab150deacdbc98d895ba815
heisenbug <heisenbug@a318f3ce-71e3-817b-b164-db38ee571ebd>
2010-11-29 17:32:05 +0000

    thrist as applicative

---
+++ purgatory/Appli.hs
@@ -0,0 +1,68 @@
+{-
+ * Copyright (c) 2010 Gabor Greif
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ -}
+
+{-# LANGUAGE GADTs, KindSignatures, EmptyDataDecls, TypeOperators #-}
+
+-- See my blog post
+--     http://heisenbug.blogspot.com/2010/11/applicative-structures-and-thrists.html
+
+module Appli where
+
+data Thrist :: (* -> * -> *) -> * -> * -> * where
+  Nil :: Thrist p a a
+  Cons :: p a b -> Thrist p b c -> Thrist p a c
+
+
+f :: Int -> Bool -> Char -> [Int]
+f i b c = if b then [i] else [0]
+
+a :: Int
+a = 42
+b = True
+c = 'A'
+
+{- Variant 0
+data Appli :: * -> * -> * where
+  Fun :: (a -> b) -> Appli (a -> b) c
+  Arg :: a -> Appli b (a -> b)
+  Par :: Thrist Appli a c -> Appli b (a -> b)
+-}
+
+data Peg
+{- Variant 1
+data Appli :: * -> * -> * where
+  Fun :: (a -> b) -> Appli (a -> b) Peg
+  Arg :: a -> Appli b (a -> b)
+  Par :: Thrist Appli a Peg -> Appli b (a -> b)
+-}
+
+-- Variant 2 ( the winner! )
+--
+data Appli :: (* -> * -> *) -> * -> * -> * where
+  Fun :: (a ~> b) -> Appli (~>) (a ~> b) Peg
+  Arg :: a -> Appli (~>) b (a ~> b)
+  Par :: Thrist (Appli (~>)) a Peg -> Appli (~>) b (a ~> b)
+
+t0 = Cons (Arg c) $ Cons (Arg b) $ Cons (Arg a) $ Cons (Fun f) Nil
+t1 = Par t0
+t2 = Cons t1 $ Cons (Fun length) Nil
