7de1945ae45f21f1f7f6d8cae7fa62f29c98b23d
heisenbug <heisenbug@a318f3ce-71e3-817b-b164-db38ee571ebd>
2012-07-10 02:40:17 +0000

    some cleanups

--- trunk/purgatory/plugin-example/If.hs
+++ trunk/purgatory/plugin-example/If.hs
@@ -2,8 +2,10 @@ module If (Interface(..),Tree(..)) where
 
 import Foreign.Ptr
 
-data Tree = Tree (Ptr Tree)
+data Tree = Tree (Ptr Tree) deriving Show
 
 data Interface = Interface {
-        processTree :: Ptr Tree -> IO (Ptr Tree)
+   processTree :: Ptr Tree -> IO (Ptr Tree)
+ , exampleTree :: Ptr Tree
+ , version :: Int
 }
